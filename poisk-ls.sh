00081914d2c7c77de39719a20725f4e5e2139255
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-28 09:41:26 +0800

    Update poisk-ls.sh

--- poisk-ls.sh
+++ poisk-ls.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
+
 echo "Enter your name:" | lolcat
 read name
-#Copy the following code to motd.sh
+#Copy the following code to jadefetch.sh
 echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" | lolcat
 echo "Welcome to Termux $name!🥀" | lolcat
 echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" | lolcat
 figlet $name | lolcat
-neofetch > motd.sh | lolcat
+jadefetch > jadefetch.sh | lolcat
 #Tries to remove motd file if any.
-rm /data/data/com.termux/files/usr/etc/motd
+rm /data/data/com.termux/files/usr/etc/jadefetch
 #Tries to remove motd.sh file if any
-rm /data/data/com.termux/files/usr/etc/motd.sh
-#Move the created motd.sh file to profile.d directory
-mv motd.sh /data/data/com.termux/files/usr/etc/profile.d
+rm /data/data/com.termux/files/usr/etc/jadefetch.sh
+#Move the created jadefetch.sh file to profile.d directory
+mv jadefetch.sh /data/data/com.termux/files/usr/etc/profile.d
 clear
 echo "Please restart Termux, to confirm changes. Enjoy😍" | lolcat
